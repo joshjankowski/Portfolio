75c941c935bbda1fa277ca6bd273b4a084949815
josh jankowski <joshjankowski@gmail.com>
2022-05-26 14:09:28 -0700

    added mobile responsiveness, fixed project links, fixed github link

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -8,3 +8,12 @@ window.transitionToPage = function(href) {
 document.addEventListener('DOMContentLoaded', function(event) {
     document.querySelector('.body').style.opacity = 1
 })
+
+// Create Responsive Mobile NavBar
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    console.log("hello govna")
+    navbarLinks.classList.toggle('active')
+} )
